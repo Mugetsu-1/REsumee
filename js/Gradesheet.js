9d8ba316eedc78f428919d788a4a9665d4ed5134
Saugat Thapa <128715197+Mugetsu-1@users.noreply.github.com>
2024-08-26 09:47:16 -0700

    Add files via upload

--- js/Gradesheet.js
+++ js/Gradesheet.js
@@ -1,9 +1,33 @@
-  
-
-    function showImage() {
-        document.querySelector('#GradesheetX').src="JS/profile1.jpg"
+function showImage1() {
+        document.querySelector('#ten').src="profile1.jpg"
     }
 
-    function hideImage(){
-        document.querySelector('#GradesheetX').src='none'
+    function hideImage1(){
+        document.querySelector('#ten').src=''
     }
+ 
+function showImage2() {
+        document.querySelector('#eleven').src="profile1.jpg"
+    }
+
+    function hideImage2(){
+        document.querySelector('#eleven').src=''
+    }
+
+function showImage3() {
+        document.querySelector('#twelve').src="profile1.jpg"
+    }
+
+    function hideImage3(){
+        document.querySelector('#twelve').src=''
+    }
+function showImage4() {
+        document.querySelector('#CTZ').src="front.jpg"
+    }
+
+    function hideImage4(){
+        document.querySelector('#CTZ').src='back.jpg'
+    }
+    
+    function RemoveImage4(){
+        document.querySelector('#CTZ').src='' }
